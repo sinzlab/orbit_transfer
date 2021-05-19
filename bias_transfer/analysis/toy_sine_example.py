Fixes bug in transferred-loader + log-prob-loss in function distance

bias_transfer/analysis/toy_sine_example.py
@@ -202,6 +202,7 @@ class ToyExampleAnalyzer(Analyzer):
         data_loaders, model, _ = TransferredTrainedModel().load_model(
             restr, include_trainer=True, include_state_dict=True, seed=seed
         )
+
         ds = data_loaders["train"]["transfer"].dataset.target_datasets
         V = ds.get("layers.6_cov_V", ds.get("layers.9_cov_V")).tensors[0].numpy()
 
@@ -222,11 +223,18 @@ class ToyExampleAnalyzer(Analyzer):
         # transfer_covariance += np.eye(n) * 0.1
         # importance = np.linalg.inv(transfer_covariance)
 
-        # What we actually use: V = V[idx[:, 0]].reshape(-1, V.shape[2])
-        V = V[idx[:, 0]].reshape(V.shape[0],-1)  # What we want to show
+        config = (Trainer() & restr).fetch1("trainer_config")
+
+        if config.get("regularization",{}).get("marginalize_over_hidden"):
+            V = V[idx[:, 0]].reshape(V.shape[0],-1)  # What we want to show
+        else:
+            V = V[idx[:, 0]].reshape(-1, V.shape[2])
+
 
         V = (V - np.mean(V, axis=1, keepdims=True)) / math.sqrt(V.shape[1])
 
         transfer_covariance = V @ V.T
+
+        transfer_covariance += np.eye(V.shape[0]) * config.get("regularization",{}).get("cov_eps",0.0)
         print(transfer_covariance)
         return transfer_covariance

bias_transfer/dataset/transferred_loader.py
@@ -67,10 +67,9 @@ def transferred_dataset_loader(
             transfer_data_loader = torch.utils.data.DataLoader(
                 dataset=transfer_dataset,
                 batch_size=main_data_loader.batch_size,
-                sampler=main_data_loader.sampler,
                 num_workers=main_data_loader.num_workers,
                 pin_memory=main_data_loader.pin_memory,
-                shuffle=False,
+                shuffle=True,
             )
             data_loaders["train"]["transfer"] = transfer_data_loader
         else:  # we don't have input data -> only targets that are presented in parallel to class-labels

bias_transfer/trainer/main_loop_modules/function_regularization/function_distance.py
@@ -15,8 +15,11 @@ class FunctionDistance(RepresentationRegularization):
         self.T = self.config.regularization.get("softmax_temp", 1.0)
         self.use_softmax = self.config.regularization.get("use_softmax", True)
         self.eps = self.config.regularization.get("cov_eps", 1e-12)
-        self.marginalize_over_hidden = self.config.regularization.get("marginalize_over_hidden", True)
+        self.marginalize_over_hidden = self.config.regularization.get(
+            "marginalize_over_hidden", True
+        )
         self.regularize_mean = self.config.regularization.get("regularize_mean", True)
+        self.add_determinant = self.config.regularization.get("add_determinant", False)
 
     def pre_forward(self, model, inputs, task_key, shared_memory):
         self.inputs = inputs.flatten()
@@ -39,10 +42,17 @@ class FunctionDistance(RepresentationRegularization):
         if V is not None:
             rep_name_ = rep_name.replace(".", "__")
 
-            if self.marginalize_over_hidden:
-                V = V.reshape((V.shape[0],-1)).double()
+            if self.regularize_mean:
+                d = output - target
             else:
-                V = V.reshape((-1, V.shape[2])).double()
+                d = output
+
+            if self.marginalize_over_hidden:
+                V = V.reshape((V.shape[0], -1)).double()  # N x (MD)
+                d = d.mean(dim=1).double()
+            else:
+                V = V.reshape((-1, V.shape[2])).double()  # ND x M
+                d = d.reshape(-1).double()
             V = (V - torch.mean(V, dim=1, keepdim=True)) / math.sqrt(V.shape[1])
 
             if hasattr(self.trainer.model, f"{rep_name_}_cov_lambdas"):
@@ -52,48 +62,37 @@ class FunctionDistance(RepresentationRegularization):
             else:
                 lambdas = torch.ones(V.shape[-1], device=self.device).double()
 
-            eps_inv = 1 / self.eps
-            importance = eps_inv * torch.eye(
-                V.shape[0], device=self.device
-            ).double() - (eps_inv * V) @ torch.inverse(
-                torch.diag(1 / lambdas) + (V.T * eps_inv) @ V
-            ) @ (
-                V.T * eps_inv
-            )
-            importance = importance.type(torch.FloatTensor).to(self.device)
+            if V.shape[0] > V.shape[1]:
+                eps_inv = 1 / self.eps
+                fd_loss = (d * eps_inv) @ d.T
+                fd_loss -= (
+                    d
+                    @ (eps_inv * V)
+                    @ torch.inverse(torch.diag(1 / lambdas) + (V.T * eps_inv) @ V)
+                    @ (V.T * eps_inv)
+                    @ d.T
+                )
+                if self.add_determinant:
+                    n, m = V.shape
+                    logdet = n * math.log(self.eps) + torch.logdet(
+                        torch.eye(m, device=self.device) + (V.T * 1 / self.eps) @ V
+                    )
 
-            # n = output.shape[0]
-            # covariance = torch.zeros((n,n), device=self.device)
-            # for i in range(n):
-            #     # covariance[i, :] = torch.exp(-2 * torch.sin(math.pi * (self.inputs[i] - self.inputs)) ** 2)
-            #     covariance[i,:] = torch.cos(self.inputs[i] - self.inputs)
-            # covariance += torch.eye(n, device=self.device) * 0.1
-            # importance = torch.inverse(covariance)
-
-            if self.regularize_mean:
-                d = output - target
+                    fd_loss += logdet  # + n * math.log(2 * math.pi)
             else:
-                d = output
-
-            if self.marginalize_over_hidden:
-                d = d.sum(dim=1)
-            else:
-                d = d.reshape(-1)
-
-            fd_loss = d @ importance @ d.T
-            # loss_without_det = fd_loss
-
-            # compute determinant:
-            # n, m = V.shape
-            # logdet = math.log(self.eps ** n) + torch.logdet(
-            #     torch.eye(m, device=self.device) + (V.T * eps_inv) @ V
-            # )
-            #
-            # fd_loss += logdet + n * math.log(2 * math.pi)
-
-            # print("Regularizer: ", loss_without_det.item(), 0.5 * fd_loss.item(), logdet.item())
-
-            return fd_loss / V.shape[0]  # normalize by (train_samples * out_classes)
+                importance = torch.inverse(
+                    V @ V.T
+                    + self.eps * torch.eye(V.shape[0], device=self.device).double()
+                )
+                fd_loss = d @ importance @ d.T
+                if self.add_determinant:
+                    n, m = V.shape
+                    fd_loss += torch.logdet(
+                        torch.eye(n, device=self.device) * self.eps + V @ V.T
+                    )
+            return (
+                fd_loss.float() / V.shape[0]
+            )  # normalize by (train_samples * out_classes)
         var = targets.get(f"{rep_name}_var")
         if var is not None:
             importance = 1 / (var + self.eps)